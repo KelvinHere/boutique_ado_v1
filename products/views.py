a31b34eebb9e75b5870f67f92528a6326df87bb8
Kelvin <kwardeu@gmail.com>
2020-12-27 15:52:29 +0000

    FEAT: Add sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -21,6 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey == 'category_name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -52,7 +54,7 @@ def all_products(request):
     # Things to pass to the page
     context = {
         'products' : products,
-        'search' : query,
+        'search_term' : query,
         'current_categories' : categories,
         'current_sorting' : current_sorting
     }
