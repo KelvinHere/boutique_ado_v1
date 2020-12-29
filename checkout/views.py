c88069d16d625dca5a26a65ebb2d3f5c3039c758
Kelvin <kwardeu@gmail.com>
2020-12-29 21:17:44 +0000

    FEAT: Add checkout success logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from bag.contexts import bag_contents
+
 
 import stripe
 
@@ -12,27 +15,79 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    # Grab the bag session
-    bag = request.session.get('bag', {})
-    if not bag:
-        # If no bag (user has input url manually) error message and redirect to products
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        # Post request
+        # Grab the bag session
+        bag = request.session.get('bag', {})
 
-    # Grab current bag and get its grand total
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    # Convert total to integer for stripe
-    stripe_total = round(total * 100)
-    # Set secret key on stripe object
-    stripe.api_key = stripe_secret_key
-    # Create payment intent
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):  # If product does not have sizes
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:  # If product has sizes
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()                            
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag was not found in our database."
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form, \ Please doublecheck your information')
+    else:
+        # Get request
+        # Grab the bag session
+        bag = request.session.get('bag', {})
+        if not bag:
+            # If no bag (user has input url manually) error message and redirect to products
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
+
+        # Grab current bag and get its grand total
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        # Convert total to integer for stripe
+        stripe_total = round(total * 100)
+        # Set secret key on stripe object
+        stripe.api_key = stripe_secret_key
+        # Create payment intent
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing, did you forget to set it in your env?')
@@ -42,6 +97,25 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')  # get checkbox if user wanted to save info
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \ Your order number is {order_number}.  A confirmation email will be sent to {order.email}')
+
+    if 'bag' in request.session:
+        del request.session['bag']  # Remove bag after succsessful processing
+    
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
