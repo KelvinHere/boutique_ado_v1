ff45ec96e566736b560579329aab9b8ba40aeb8a
Kelvin <kwardeu@gmail.com>
2021-01-01 11:41:31 +0000

    FEAT: Add class methods towebhook handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
-    """ Handle Stripe Webhooks """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
