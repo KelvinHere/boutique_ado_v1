a4f961459e5be7ea2eb8e8c73bc4f17ec34b00f3
Kelvin <kwardeu@gmail.com>
2021-01-02 15:35:19 +0000

    MINOR: Fix image field in admin

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add/', views.add_product, name='add_product'),
     # Product detail specifies int so when we go to /product/add it will
     # not try to look for a product called 'add' and will allow the add/
     # view to be called instead as django will use the first url it finds
