f3eef1597f201a7b9b0c6e2ac80392348769c8f3
Kelvin <kwardeu@gmail.com>
2021-01-02 14:51:08 +0000

    FEAT: Edit product

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     # Product detail specifies int so when we go to /product/add it will
     # not try to look for a product called 'add' and will allow the add/
     # view to be called instead as django will use the first url it finds
